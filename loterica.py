a48022929f73808e2ac27b86547e0d374d28d8cd
Luiz Celso <dev@limabauru.com.br>
2021-10-23 09:26:43 -0300

    Carregamento Json

--- loterica.py
+++ loterica.py
@@ -1,16 +1,32 @@
+from typing import final
 import requests, pprint, json, sys
+from datetime import datetime
+
+from requests import structures
 
 PARTE1 = 'http://loterias.caixa.gov.br'
 PARTE2 = 'pw/Z7_HGK818G0KG4QF0QLDEU6PK2084/res/id=buscaResultado/c=cacheLevelPage//p=concurso='
 PARTE3 = '?timestampAjax=1634917125537'
 args = sys.argv[1:][0]
 
-response = requests.get('http://loterias.caixa.gov.br/wps/portal/loterias/landing/federal')
+try:
 
-conteudo_site = response.headers['IBM-Web2-Location']
+    response = requests.get('http://loterias.caixa.gov.br/wps/portal/loterias/landing/federal')
 
-url = PARTE1 + conteudo_site + PARTE2 + args + PARTE3
 
-response = requests.get(url)
+    conteudo_site = response.headers['IBM-Web2-Location']
 
-pprint.pprint(json.loads(response.content))
+    url = PARTE1 + conteudo_site + PARTE2 + args + PARTE3
+
+    response = requests.get(url)
+
+    #print(response.content)
+    byte_response = response.content
+
+    string_json = "[" + byte_response.decode('utf8')+ "]"
+
+    final_json = json.loads(string_json)
+    for item in final_json:
+        print(item['dezenasSorteadasOrdemSorteio'])
+except Exception as e:
+    print(e)
